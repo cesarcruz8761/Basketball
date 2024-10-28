5c54a645e7acf303c413d3db005476a32c245a2b
Cesar Cruz <cesarcruz8761@gmail.com>
2024-10-28 17:23:34 -0400

    will code show up?

---
+++ basketball.py
@@ -0,0 +1,11 @@
+    """ to calculate basketball stats"""
+    
+    
+class baskball
+    """ new game later.
+    
+    Attributes:
+        name(str): the team name'
+        connections(set of Baskball): other plays of the 
+            night.
+    """
